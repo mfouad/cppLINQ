cdc39a41d74809131fd9a67105e1cb9f6d46548b
mfouad <m8fouad@gmail.com>
2015-03-14 21:20:00 +0200

    Template support
    supports any container of any type of elements to be queried

---
+++ ImmediateEval.h
@@ -0,0 +1,78 @@
+#include <vector>
+#include <list>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <functional>
+#include <assert.h>
+using namespace std;
+
+
+template <class container>
+class ImmediateQuery
+{
+	
+	
+	// Types
+	typedef typename container::value_type value_type;	// the type of the collection elements. e.g. "int" in "vector<int>"
+	typedef std::function<bool(value_type&)> Predicate;	// The predicate function (or lambda) that will be used to filter elements
+
+	list<value_type> m_result;									// a temp container of the same type, so that we don't alter the original container
+
+public:
+
+	ImmediateQuery(const container& collection)
+	{
+		// Copy to a list
+		m_result.insert(m_result.begin(), collection.begin(), collection.end());
+	}
+
+	//template <typename param>
+	
+	ImmediateQuery& Where(Predicate predicate)
+	{
+		// removes all the items that do not satisfy the predicate .. 
+		remove_if(m_result.begin(), m_result.end(), std::not1(predicate));
+		return *this;
+	}
+
+	bool any(Predicate& predicate)
+	{
+		return std::any_of(m_result.begin(), m_result.end(), predicate);
+	}
+
+
+	// Evaluators (terminal operations that terminate the query and evaluates it)
+	void print(Predicate& predicate)
+	{
+		std::for_each(m_result.begin(), m_result.end(), [](int& e) { if (predicate(e)) cout << e << " "; });
+		cout << endl;
+	}
+
+	void print()
+	{
+		std::for_each(m_result.begin(), m_result.end(), [](int& e) { cout << e << " "; });
+		cout << endl;
+	}
+
+	int count(Predicate& predicate)
+	{
+		return std::count_if(m_result.begin(), m_result.end(), predicate);
+	}
+
+	int count()
+	{
+		return m_result.size();
+	}
+
+};
+
+
+template <typename container>
+ImmediateQuery<container> From(container& collection)
+{
+	return ImmediateQuery<container>(collection);
+}
+
+
--- main.cpp
+++ main.cpp
@@ -1,78 +1,23 @@
-#include <vector>
-#include <list>
-#include <queue>
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <functional>
-using namespace std;
+#include "ImmediateEval.h"
 
-typedef vector<int> container_type;
-
-template <typename T>
-class Command
+void TestImmediateEvaluation()
 {
-	list<T> m_result;
+	vector<int> v{ 0, 1, 2, 3, 4 };
+
+	From(v).Where([](int& e){ return (e == 1); }).print();
 	
-public:
+	assert(From(v).count() == 5);
+	From(v).Where([](int& e){ return (e == 1); }).count();
 
-	Command(const vector<T>& collection)
-	{
-		// Copy to a list
-		m_result.insert(m_result.begin(), collection.begin(), collection.end());
-	}
+//	From(v).Where([](int& e){ return (e == 1); }).any([](int& e){return (e == 1); });
+	//*/
+}
 
-	//template <typename param>
-	Command& Where(std::function<bool(T&)> predicate)
-	{
-		// removes all the items that do not satisfy the predicate .. 
-		m_result.remove_if(std::not1(predicate));
-		return *this;
-	}
-
-	template <class Predicate>
-	bool any(Predicate& predicate)
-	{
-		return std::any_of(m_result.begin(), m_result.end(), predicate);
-	}
-
-
-	// Evaluators (terminal operations that terminate the query and evaluates it)
-	template <class Predicate>
-	void print(Predicate& predicate)
-	{
-		std::for_each(m_result.begin(), m_result.end(), [](int& e) { if (predicate(e)) cout << e << endl; });
-	}
-
-	void print()
-	{
-		std::for_each(m_result.begin(), m_result.end(), [](int& e) { cout << e << endl; });
-	}
-
-	template <class Predicate>
-	int count(Predicate& predicate)
-	{
-		return std::count_if(m_result.begin(), m_result.end(), predicate);
-	}
-
-	int count()
-	{
-		return m_result.size();
-	}
-
-};
 
 
 int main()
 {
-	vector<int> v{ 0, 1, 2, 3, 4 };
-
-	Command<int>(v).Where([](int& e){ return (e == 1); }).print();
-
-	cout << Command<int>(v).Where([](int& e){ return (e == 1); }).count();
-
-
-	cout << Command<int>(v).Where([](int& e){ return (e == 1); }).any([](int& e){return (e == 1); });
+	TestImmediateEvaluation();
 
 	return 0;
 }
